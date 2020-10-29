f23a7348b2da3d04c03b7070bcf983b3162402f7
Tushar Saxena <stushar345@gmail.com>
2020-10-29 12:04:13 +0530

    Add files via upload

---
+++ two.cpp
@@ -0,0 +1,16 @@
+void mirror(Node* node) 
+{
+     if (node == NULL)  
+        return;  
+    else
+    { 
+        struct Node* temp; 
+          
+        mirror(node->left); 
+        mirror(node->right); 
+
+        temp     = node->left; 
+        node->left = node->right; 
+        node->right = temp; 
+    } 
+}
