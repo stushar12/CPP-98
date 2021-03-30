15d818d2ebcce5ece597ab060b4a91b7c5fdbd6e
Tushar Saxena <stushar345@gmail.com>
2021-03-31 00:00:14 +0530

    Update two.cpp

--- two.cpp
+++ two.cpp
@@ -3,14 +3,15 @@ Node* mirrorTree(Node* root)
     if (root == NULL)
         return root;
      
+    
+     if (root->left)
+        mirrorTree(root->left);
+    if (root->right)
+        mirrorTree(root->right);
+    
     Node* temp = root->left;
     root->left = root->right;
     root->right = temp;
  
-    if (root->left)
-        mirrorTree(root->left);
-    if (root->right)
-        mirrorTree(root->right);
-   
   return root;
 }
